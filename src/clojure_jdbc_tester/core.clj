1ce6a3edada7d3a8ac6d610c48e74cdd6011ada0
MokkeMeguru <meguru.mokke@gmail.com>
2021-04-09 18:06:57 +0900

    first commit

--- src/clojure_jdbc_tester/core.clj
+++ src/clojure_jdbc_tester/core.clj
@@ -1,29 +1,13 @@
 (ns clojure-jdbc-tester.core
   (:gen-class)
   (:require [environ.core :refer [env]]
-            [clojure.java.jdbc :as jdbc]))
-
-(defn build-connection-options-postgres []
-  {:dbtype "postgresql"
-   :dbname (env :database-name)
-   :host (env :database-host)
-   :port (env :database-port)
-   :user (env :database-user)
-   :password (env :database-user-password)})
-
-(defn build-connection-options [database-type]
-  (cond
-    (= database-type "postgresql") (build-connection-options-postgres)
-    :else (throw (ex-info "Unknown database-type" {:database-type "database-type"}))))
+            [clojure-jdbc-tester.proxy :as cjt-proxy]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [database-type (env :database-type)
-        connection-options (build-connection-options database-type)]
-    (println "check your settings ...")
-    (try
-      (println (jdbc/get-connection connection-options))
-      (println "Your settings is correct. have a fun to use Google Cloud SQL!")
-      (catch Exception e
-        (println "Your settings is not correct..." (.getMessage e))))))
+  (let [connection-method (env :connection-method)]
+    (cond
+      (= connection-method "proxy") (cjt-proxy/-main)
+      (= connection-method "socket-factory") (println "oh")
+      :else (throw (ex-info "unknown method" {:connection-method connection-method})))))
---
+++ src/clojure_jdbc_tester/proxy.clj
@@ -0,0 +1,28 @@
+(ns clojure-jdbc-tester.proxy
+  (:require [environ.core :refer [env]]
+            [clojure.java.jdbc :as jdbc]))
+
+(defn build-connection-options-postgres []
+  {:dbtype "postgresql"
+   :dbname (env :database-name)
+   :host (env :database-host)
+   :port (env :database-port)
+   :user (env :database-user)
+   :password (env :database-user-password)})
+
+(defn build-connection-options [database-type]
+  (cond
+    (= database-type "postgresql") (build-connection-options-postgres)
+    :else (throw (ex-info "Unknown database-type" {:database-type "database-type"}))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (let [database-type (env :database-type)
+        connection-options (build-connection-options database-type)]
+    (println "check your settings ...")
+    (try
+      (println (jdbc/get-connection connection-options))
+      (println "Your settings is correct. have a fun to use Google Cloud SQL!")
+      (catch Exception e
+        (println "Your settings is not correct..." (.getMessage e))))))
